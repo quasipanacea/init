94411eefe661471871e25fef20a6a6c45fc3c39f
Edwin Kofler <edwin@kofler.dev>
2022-09-19 16:17:55 -0700

    tweak: Improvements

--- Bakefile.sh
+++ Bakefile.sh
@@ -8,41 +8,37 @@ task.init() {
 	mkdir -p './repos'
 	
 	for repo in "${global_repos[@]}"; do
-		util.clone_and_symlink "$repo"
+		util.clone "$repo"
 	done
 
-	for dir in server-deno client-web webext-broker; do
+	for dir in 'server-deno' 'client-web' 'webext-broker'; do
 		ln -sfT "$BAKE_ROOT/repos/common" "$BAKE_ROOT/repos/$dir/common"
 	done
 }
 
 task.update() {
 	for repo in "${global_repos[@]}"; do
-		(
-			cd "./repos/$repo"
+		pushd "./repos/$repo"
 			
-			if [ -n "$(git status -s)" ]; then
-				bake.die "There are unstaged changes in repo: $repo"
-			fi
+		if [ -n "$(git status -s)" ]; then
+			bake.die "There are unstaged changes in repo: $repo"
+		fi
 
-			git pull origin main
-		)
+		git pull origin main
+		
+		popd
 	done
 }
 
 task.dev() {
 	code './kaxon.code-workspace'
-	
 }
 
-util.clone_and_symlink() {
+util.clone() {
 	local repo_name="$1"
 
-	if [ ! -d "$BAKE_ROOT/../$repo_name" ]; then
+	if [ ! -d "$BAKE_ROOT/repos/$repo_name" ]; then
 		bake.info "Cloning: project-kaxon/$repo_name"
-		git clone "git@github.com:project-kaxon/$repo_name" "$BAKE_ROOT/../$repo_name"
+		git clone "git@github.com:project-kaxon/$repo_name" "$BAKE_ROOT/repos/$repo_name"
 	fi
-
-	bake.info "Symlinking: ./repos/$repo_name"
-	ln -Tsf "$BAKE_ROOT/../$repo_name" "./repos/$repo_name"
 }
